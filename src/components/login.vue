25f33a4af925eebea07dc5b18d33a5b5fb6acb20
v-yui <1356674019@qq.com>
2020-04-26 19:07:26 +0800

    处理element ui

---
+++ src/components/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login_cotainer">
+    <el-form  label-width="80px">
+      <el-form-item label="活动名称">
+        <el-input></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+</script>
+<style scoped>
+</style>
---
+++ src/plugins/element.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import { Form, FormItem, Button, Input } from 'element-ui'
+
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Button)
+Vue.use(Input)
